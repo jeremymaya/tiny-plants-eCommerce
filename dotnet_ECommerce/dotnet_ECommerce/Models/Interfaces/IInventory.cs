5a6df359f253ad82f1c0df28301ff238c30c391e
Karina Chen <karina6188@gmail.com>
2019-11-20 13:35:00 -0800

    Created an interface for product inventory

---
+++ dotnet_ECommerce/dotnet_ECommerce/Models/Interfaces/IInventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_ECommerce.Models.Interfaces
+{
+    interface IInventory
+    {
+        // Create
+        Task CreateInventoryAsync(Product product);
+
+        // Read - Get All
+        Task<List<Product>> GetAllInventoriesAsync();
+
+        // Read - Gey By ID
+        Task<Product> GetInventoryByIdAsync(int id);
+
+        // Update
+        Task UpdateInventoryAsync(Product product);
+
+        // Delete
+        Task RemoveInventoryAsync(int id);
+    }
+}
