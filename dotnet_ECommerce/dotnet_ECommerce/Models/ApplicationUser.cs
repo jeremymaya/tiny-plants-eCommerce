9817948c6fe3c508dceca2654126b242253df1a3
Kyungrae <jeremiah.kim@hotmail.com>
2019-11-20 13:19:25 -0800

    ApplicationUser class completed

---
+++ dotnet_ECommerce/dotnet_ECommerce/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace dotnet_ECommerce.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
