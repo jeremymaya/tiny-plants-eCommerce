2670a0d47d30441760fbda410df47afd6d08b8c2
Karina Chen <karina6188@gmail.com>
2019-11-20 17:27:41 -0800

    solve merge conflict

--- dotnet_ECommerce/dotnet_ECommerce/Startup.cs
+++ dotnet_ECommerce/dotnet_ECommerce/Startup.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_ECommerce.Data;
+using dotnet_ECommerce.Models;
 using dotnet_ECommerce.Models.Interfaces;
 using dotnet_ECommerce.Models.Interfaces.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -19,8 +22,12 @@ namespace dotnet_ECommerce
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
             services.AddScoped<IInventory, ProductService>();
 
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                 .AddEntityFrameworkStores<ApplicationDbContext>()
+                 .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
