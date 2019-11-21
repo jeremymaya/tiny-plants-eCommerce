b3350d6dac77888bd446b28af94af5e069fd2363
Kyungrae <jeremiah.kim@hotmail.com>
2019-11-20 19:26:52 -0800

    ApplicationDbContext completed

--- dotnet_ECommerce/dotnet_ECommerce/Data/ApplicationDbContext.cs
+++ dotnet_ECommerce/dotnet_ECommerce/Data/ApplicationDbContext.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using dotnet_ECommerce.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnet_ECommerce.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
     }
