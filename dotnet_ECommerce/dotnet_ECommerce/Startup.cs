dc06cc93282e0b5c9511cf6fb6d074127f44812c
Karina Chen <karina6188@gmail.com>
2019-11-20 17:25:03 -0800

    register interface to startup

--- dotnet_ECommerce/dotnet_ECommerce/Startup.cs
+++ dotnet_ECommerce/dotnet_ECommerce/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_ECommerce.Models.Interfaces;
+using dotnet_ECommerce.Models.Interfaces.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -17,6 +19,8 @@ namespace dotnet_ECommerce
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddScoped<IInventory, ProductService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
