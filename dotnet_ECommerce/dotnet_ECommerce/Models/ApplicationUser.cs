45f058ea19e86cdee9feb5105a14e4e8988ea603
Karina Chen <karina6188@gmail.com>
2019-11-21 16:09:18 -0800

    add unit tests

--- dotnet_ECommerce/dotnet_ECommerce/Models/ApplicationUser.cs
+++ dotnet_ECommerce/dotnet_ECommerce/Models/ApplicationUser.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace dotnet_ECommerce.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
     }
 }
