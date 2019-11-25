0770e0251840cce305e7a60dce3d4ff13cf74ec8
Kyungrae <jeremiah.kim@hotmail.com>
2019-11-25 11:12:58 -0800

    products rendered correctly

---
+++ dotnet_ECommerce/dotnet_ECommerce/Pages/Products.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_ECommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_ECommerce.Pages
+{
+    public class ProductsModel : PageModel
+    {
+        private readonly dotnet_ECommerce.Data.StoreDbContext _context;
+
+        public ProductsModel(dotnet_ECommerce.Data.StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Products { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Products = await _context.Product
+                .ToListAsync();
+        }
+    }
+}
