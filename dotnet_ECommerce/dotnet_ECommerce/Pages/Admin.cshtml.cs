f24b88ce8a7622ad9c6ff80f4f4abb106f58e387
Kyungrae <jeremiah.kim@hotmail.com>
2019-11-26 11:16:04 -0800

    Admin dashboard layout completed

---
+++ dotnet_ECommerce/dotnet_ECommerce/Pages/Admin.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet_ECommerce.Pages
+{
+    //[Authorize(Policy="")]
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
