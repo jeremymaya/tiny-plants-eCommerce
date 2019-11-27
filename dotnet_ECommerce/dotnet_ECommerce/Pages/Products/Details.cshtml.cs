9f9fa422acee7600f89f35db99d54c4292341d22
Kyungrae <jeremiah.kim@hotmail.com>
2019-11-27 15:39:13 -0800

    fixed all issues

--- dotnet_ECommerce/dotnet_ECommerce/Pages/Products/Details.cshtml.cs
+++ dotnet_ECommerce/dotnet_ECommerce/Pages/Products/Details.cshtml.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_ECommerce.Models;
+using dotnet_ECommerce.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,29 @@ namespace dotnet_ECommerce.Pages.Products
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        /// <summary>
+        /// Dependency injection to establish a private connection to a database table by injecting an interface
+        /// </summary>
+        private readonly IInventory _context;
+        /// <summary>
+        /// A contructor to set propety to the corresponding interface instance
+        /// </summary>
+        /// <param name="context">IInventory interface</param>
+        public DetailsModel(IInventory context)
         {
+            _context = context;
+        }
+        /// <summary>
+        /// A property to be available on the Model property in the Razor Page
+        /// </summary>
+        public Product SingleProduct { get; set; }
+        /// <summary>
+        /// Asynchronous handler method to process the default GET request
+        /// </summary>
+        /// <returns>A product from the database based on the id</returns>
+        public async Task OnGetAsync(int id)
+        {
+            SingleProduct = await _context.GetInventoryByIdAsync(id);
         }
     }
 }
