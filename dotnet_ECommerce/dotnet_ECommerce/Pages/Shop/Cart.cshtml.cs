a1bd87c8bad1c7fb79cea62563cc4f33c6bd5424
Karina Chen <karina6188@gmail.com>
2019-11-30 18:14:48 -0800

    fix add to cart button only available to signin users

--- dotnet_ECommerce/dotnet_ECommerce/Pages/Shop/Cart.cshtml.cs
+++ dotnet_ECommerce/dotnet_ECommerce/Pages/Shop/Cart.cshtml.cs
@@ -12,24 +12,6 @@ namespace dotnet_ECommerce.Pages.Shop
 {
     public class CartModel : PageModel
     {
-        /// <summary>
-        /// Dependency injection to establish a private connection to a database table by injecting an interface
-        /// </summary>
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly IInventory _inventory;
-        private readonly IShop _shop;
-
-        /// <summary>
-        /// A contructor to set propety to the corresponding interface instance
-        /// </summary>
-        /// <param name="inventory">IInventory interface</param>
-        public CartModel(IInventory inventory, IShop shop, UserManager<ApplicationUser> userManager)
-        {
-            _userManager = userManager;
-            _inventory = inventory;
-            _shop = shop;
-        }
-
         public void OnGet()
         {
         }
