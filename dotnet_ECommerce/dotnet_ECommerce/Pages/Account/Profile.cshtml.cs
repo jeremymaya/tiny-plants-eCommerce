3c898bdff494d7e962ed4cf2d10e3033891e1ec5
Kyungrae <jeremiah.kim@hotmail.com>
2019-12-07 12:40:33 -0800

    User Profile completed

---
+++ dotnet_ECommerce/dotnet_ECommerce/Pages/Account/Profile.cshtml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_ECommerce.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_ECommerce.Pages.Account
+{
+    public class ProfileModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileModel(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public ApplicationUser CurrentUser { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            CurrentUser = await _userManager.GetUserAsync(User);
+        }
+
+        public async Task<RedirectToPageResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = await _userManager.GetUserAsync(User);
+
+                user.FirstName = Request.Form["FirstName"];
+                user.LastName = Request.Form["LastName"];
+                user.Address = Request.Form["Address"];
+                user.Address2 = Request.Form["Address2"];
+                user.City = Request.Form["City"];
+                user.State = Request.Form["State"];
+                user.Zip = Request.Form["Zip"];
+
+                try
+                {
+                    await _userManager.UpdateAsync(user);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw;
+                }
+            }
+            return RedirectToPage();
+        }
+
+        public class ProfileInput
+        {
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            public string Address { get; set; }
+
+            [Display(Name = "Address 2")]
+            public string Address2 { get; set; }
+
+            [Required]
+            public string City { get; set; }
+
+            [Required]
+            public string State { get; set; }
+
+            [Required]
+            [DataType(DataType.PostalCode)]
+            [Compare("Zip", ErrorMessage = "The is an invalid zip code")]
+            public string Zip { get; set; }
+        }
+    }
+}
