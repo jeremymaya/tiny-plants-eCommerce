6a854bcff0f95b27ec6cb388bde08ce94edcc6f9
Kyungrae <jeremiah.kim@hotmail.com>
2019-12-11 17:30:33 -0800

    Azure Blob Storage intergrated into /Create

--- dotnet_ECommerce/dotnet_ECommerce/Controllers/InventoryController.cs
+++ dotnet_ECommerce/dotnet_ECommerce/Controllers/InventoryController.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using dotnet_ECommerce.Models;
 using dotnet_ECommerce.Models.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,9 +21,12 @@ namespace dotnet_ECommerce.Controllers
     {
         private readonly IInventory _context;
 
-        public InventoryController(IInventory inventory)
+        public Blob Blob { get; }
+
+        public InventoryController(IInventory inventory, IConfiguration configuration)
         {
             _context = inventory;
+            Blob = new Blob(configuration);
         }
 
         // GET: Inventory
@@ -72,10 +79,22 @@ namespace dotnet_ECommerce.Controllers
         /// <returns>Index.cshtml with the updated inventory list from the the conntected database</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Sku,Name,Price,Description,Image")] Product product)
+        public async Task<IActionResult> Create(Product product)
         {
             if (ModelState.IsValid)
             {
+                var filePath = Path.GetTempFileName();
+                CloudBlobContainer blobContainer = await Blob.GetContainer("products");
+
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    await product.File.CopyToAsync(stream);
+                }
+
+                await Blob.UploadFile(blobContainer, product.File.FileName, filePath);
+
+                product.Image = Blob.GetBlob(product.File.FileName, "products").Uri.AbsoluteUri;
+
                 await _context.CreateInventoryAsync(product);
                 return RedirectToAction(nameof(Index));
             }
@@ -181,6 +200,7 @@ namespace dotnet_ECommerce.Controllers
             }
 
             await _context.RemoveInventoryAsync(id);
+
             return RedirectToAction(nameof(Index));
         }
 
--- dotnet_ECommerce/dotnet_ECommerce/Migrations/20191211181706_initial.cs
+++ dotnet_ECommerce/dotnet_ECommerce/Migrations/20191211181706_initial.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace dotnet_ECommerce.Migrations
 {
-    public partial class intialA : Migration
+    public partial class initial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
