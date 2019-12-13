2ac1aa847dd32e3b24df9e1baf2c525150d2f956
Kyungrae <jeremiah.kim@hotmail.com>
2019-12-12 17:34:13 -0800

    Order Details completed

---
+++ dotnet_ECommerce/dotnet_ECommerce/Pages/Orders/Detail.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_ECommerce.Models;
+using dotnet_ECommerce.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet_ECommerce.Pages.Orders
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IOrder _order;
+
+        public DetailModel(IOrder order)
+        {
+            _order = order;
+        }
+
+        public IEnumerable<OrderItems> OrderItems { get; set;}
+
+        public async Task OnGetAsync(int id)
+        {
+            OrderItems = await _order.GetOrderItemsByOrderIdAsync(id);
+        }
+    }
+}
