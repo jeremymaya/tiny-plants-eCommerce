b4b6d80ba7ca4a51f69d29c22f6b9738d00e50e8
Karina Chen <karina6188@gmail.com>
2019-12-12 16:45:59 -0800

    add Order page for user

---
+++ dotnet_ECommerce/dotnet_ECommerce/Pages/Account/Order.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_ECommerce.Models;
+using dotnet_ECommerce.Models.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace dotnet_ECommerce.Pages.Account
+{
+    public class OrderModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly IOrder _order;
+
+        /// <summary>
+        /// A property that brings in SignInManager depdency to be used in the class
+        /// </summary>
+        /// <param name="signInManager">SignInManager context</param>
+        public OrderModel(UserManager<ApplicationUser> userManager, IOrder order)
+        {
+            _userManager = userManager;
+            _order = order;
+        }
+
+        /// <summary>
+        /// A property to be available on the Model property in the Razor Page
+        /// </summary>
+        public IEnumerable<Order> Orders { get; set; }
+
+        public async Task OnGet()
+        {
+            ApplicationUser user = await _userManager.GetUserAsync(User);
+            Orders = await _order.GetOrdersByUserIdAsync(user.Id);
+        }
+    }
+}
